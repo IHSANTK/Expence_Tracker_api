77293d31d4449221990750c05aa4310a998d074c
Ihsan <ihsantk786313@gmail.com>
2024-06-25 14:56:29 +0530

    expencetracker

--- routes/swaggerDoc.js
+++ routes/swaggerDoc.js
@@ -1,7 +1,7 @@
 
 /**
  * @swagger
- * /signin:
+ * /signup:
  *   post:
  *     summary: Create a new user
  *     tags: [Users]
@@ -68,10 +68,15 @@
  *   put:
  *     summary: Update a user
  *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
  *     requestBody:
  *       required: true
  *       content:
  *         application/json:
+ * 
  *           schema:
  *             type: object
  *             required:
@@ -91,6 +96,215 @@
  *       500:
  *         description: Server error
  */
+
+
+/**
+ * @swagger
+ * /addexpenses/{userId}:
+ *   post:
+ *     summary: Add expenses
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - amount
+ *             properties:
+ *               name:
+ *                 type: string  
+ *               amount:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Expense added successfully
+ *       500:
+ *         description: Server error
+ */
+
+
+/**
+ * @swagger
+ * /updateexpense/{userId}/{expenseId}:
+ *   put:
+ *     summary: Update expense
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *       - in: path
+ *         name: expenseId
+ *         required: true
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - amount
+ *               - date
+ *             properties:
+ *               name:
+ *                 type: string  
+ *               amount:
+ *                 type: string
+ *               date:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Expense updated successfully
+ *       500:
+ *         description: Server error
+ */
+
+/**
+ * @swagger
+ * /deleteexpense/{userId}/{expenseId}:
+ *   delete:
+ *     summary: Delete expense
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *       - in: path
+ *         name: expenseId
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Expense updated successfully
+ *       500:
+ *         description: Server error
+ */
+
+
+
+/**
+ * @swagger
+ * /addincome/{userId}:
+ *   post:
+ *     summary: Add income
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - amount
+ *             properties:
+ *               name:
+ *                 type: string  
+ *               amount:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Income added successfully
+ *       500:
+ *         description: Server error
+ */
+
+
+/**
+ * @swagger
+ * /updateincome/{userId}/{expenseId}:
+ *   put:
+ *     summary: Update income
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the user
+ *       - in: path
+ *         name: expenseId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the income
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *               - amount
+ *               - date
+ *             properties:
+ *               name:
+ *                 type: string  
+ *               amount:
+ *                 type: string
+ *               date:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Income updated successfully
+ *       500:
+ *         description: Server error
+ */
+
+/**
+ * @swagger
+ * /deleteincome/{userId}/{expenseId}:
+ *   delete:
+ *     summary: Delete expense
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *       - in: path
+ *         name: expenseId
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Income updated successfully
+ *       500:
+ *         description: Server error
+ */
+
+/**
+ * @swagger
+ * /getuser/{userId}:
+ *   get:
+ *     summary: get user
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *       
+ *     responses:
+ *       200:
+ *         description: Get user
+ *       500:
+ *         description: Server error
+ */
+
 
 
 /**
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const UserController = require('../controllers/UserController');
 const {authenticate} = require('../middlewear/jwt')
 
-router.post('/signin', UserController.createUser);
+router.post('/signup', UserController.createUser);
 router.post('/login',UserController.loginuser)
 router.get('/getuser/:id', UserController.getUserById);
 router.put('/updateuser/:id', UserController.updateUser);
@@ -22,5 +22,8 @@ router.delete('/deleteincome/:id/:incomeId',authenticate,UserController.deleteIn
 
 router.get('/expensesum/:id',authenticate,UserController.getExpensesSum);
 router.get('/:id/income/sum',authenticate,UserController.getIncomeSum);
+
+
+
 
 module.exports = router;
